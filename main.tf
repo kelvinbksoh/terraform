926d16c83dc3a748127a33c2198adfc2fffe4799
ofnt <lv4alacrity@gmail.com>
2019-11-05 13:12:21 +0800

    Added bash script, keypairs and variable.tfvars

--- main.tf
+++ main.tf
@@ -11,14 +11,29 @@ provider "aws" {
 }
 
 # resource "<PROVIDER>_<TYPE>" "<NAME>" { [CONFIG ...]}
-resource "aws_instance" "vault_server" { # creating resources for aws provider
+resource "aws_instance" "web_server" { # creating resources for aws provider
   count = 1 # create 10 ec2 instances
   ami = "${lookup(var.my_ami, var.region)}"
   instance_type = "t1.micro"
-  user_data = "${file("install_vault.sh")}" # Bash script that executes when the server is booting
+  vpc_security_group_ids = [aws_security_group.web_security_group.id]
+  # user_data = "${file("install_vault.sh")}" # Bash script that executes when the server is booting
+  user_data = "${file("hello_world.sh")}"
   key_name = "mykey" # tag a key pair when creating instances
   tags = {
-      Name = "Vault Server"
+      Name = "Web Server"
+  }
+}
+
+resource "aws_security_group" "web_security_group" {
+  name = "web-server-instance"
+  # Allow EC2 Instance to receive traffic on port 8080
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+    # allow incoming TCP request on port 8080 from the CIDR block 0.0.0.0/0 (any IP).
+    # CIDR blocks are a concise way to specify IP address ranges.
   }
 }
 
---
+++ variable.tfvars
@@ -0,0 +1,8 @@
+region = "us-east-1"
+profile = "default"
+my_ami = {
+    "us-west-2" = "ami-04b762b4289fba92b"
+    # "us-east-1" = "ami-04169656fea786776"
+    # "us-east-1" = "ami-00dc79254d0461090" # Amazon Linux 2 AMI
+    "us-east-1" = "ami-04b9e92b5572fa0d1" # Ubuntu AMI
+}
